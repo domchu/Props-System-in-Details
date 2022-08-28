2d89d9c1d0cfe3d83d8ffad8697ba7a5979b8681
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-28 23:52:22 +0100

    feature: Added the structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today by 6:00pm</span>
+          </div>
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
